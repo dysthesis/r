48324bf2465291ec5131eb7aaeda63d0b5bb7b99
Dysthesis <antheoraviel@protonmail.com>
2025-06-24 13:45:18 +1000

    Parse HTTP content as RSS or Atom

--- src/content.rs
+++ src/content.rs
@@ -1,7 +1,9 @@
-use smol::fs::read_to_string;
+use std::fmt::Display;
+
 use url::Url;
 
 /// The body of an HTTP content fetched from the URL
+#[derive(Debug, Clone)]
 pub struct HttpContent(String);
 
 impl TryFrom<Url> for HttpContent {
@@ -15,5 +17,18 @@ impl TryFrom<Url> for HttpContent {
             .read_to_string()?;
 
         Ok(Self(content))
+    }
+}
+
+impl From<HttpContent> for String {
+    fn from(HttpContent(value): HttpContent) -> Self {
+        value
+    }
+}
+
+impl Display for HttpContent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let content: String = self.clone().into();
+        write!(f, "{content}")
     }
 }
--- src/feed.rs
+++ src/feed.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use atom_syndication::Feed;
 use rss::Channel;
 
@@ -11,6 +12,13 @@ impl TryFrom<HttpContent> for FeedParser {
     type Error = anyhow::Error;
 
     fn try_from(value: HttpContent) -> Result<Self, Self::Error> {
-        todo!()
+        let content = value.to_string();
+        if let Ok(channel) = rss::Channel::read_from(content.as_bytes()) {
+            Ok(FeedParser::Rss(channel))
+        } else if let Ok(feed) = atom_syndication::Feed::read_from(content.as_bytes()) {
+            Ok(FeedParser::Atom(feed))
+        } else {
+            Err(anyhow!("Cannot parse content as RSS or Atom!"))
+        }
     }
 }
